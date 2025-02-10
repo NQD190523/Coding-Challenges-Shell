7c0f7c71f3b5fac69d6515540607999fb3a9d5a6
NQD190523 <duc19052003@gmail.com>
2025-02-10 21:46:28 +0700

    Fix(CdCommand): fix directory change issue when using cd ..

--- src/commands/CdCommand.java
+++ src/commands/CdCommand.java
@@ -1,10 +1,22 @@
 package commands;
 
+import java.io.File;
+
 public class CdCommand {
 
     public static void changeDirectory(String path) {
         String currentDirectory = System.getProperty("user.dir");
-        String newDirectory = currentDirectory + "\\" + path;
-        System.setProperty("user.dir", newDirectory);
+        File currentDir = new File(currentDirectory);
+        File newDir;
+        if(path.equals("..")) {
+             newDir = currentDir.getParentFile();
+        } else {
+            newDir = new File(currentDirectory + "\\" + path);
+        }
+        if(newDir.exists() && newDir.isDirectory() && newDir != null) {
+            System.setProperty("user.dir", newDir.getAbsolutePath());
+        } else {
+            System.out.println("Directory not found: " + path);
+        }
     }
 }
